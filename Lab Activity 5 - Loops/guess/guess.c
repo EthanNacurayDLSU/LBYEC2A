77cbd653da88f26fb0182824ed518045500ad035
EthanNacurayDLSU <ethan_nacuray@dlsu.edu.ph>
2025-02-24 10:22:41 +0800

    fix

--- Lab Activity 5 - Loops/guess/guess.c
+++ Lab Activity 5 - Loops/guess/guess.c
@@ -2,24 +2,25 @@
 #include <ctype.h> //to use toupper or tolower
 int main()
 {
-    char guess, actual='E';
+    char guess, actual = 'E';
 
     do
     {
-        printf ("\nenter your guess : ");
+        printf("\nenter your guess : ");
         scanf("%c", &guess);
 
         if (toupper(guess) < actual)
         {
-            printf ("Your guess is lower than my letter\n");
-        } else if (toupper(guess) > actual)
-        {
-            printf ("Your guess is higher than my letter\n");
+            printf("Your guess is lower than my letter\n");
         }
-       // getchar(); //clears buffer so scanf doesnt break
+        else if (toupper(guess) > actual)
+        {
+            printf("Your guess is higher than my letter\n");
+        }
+        // getchar(); //clears buffer so scanf doesnt break
     } while (toupper(guess) != actual);
 
-printf ("YOU ARE CORRECT, MY LETTER IS %c", actual);
-    
-    return 0;w
+    printf("YOU ARE CORRECT, MY LETTER IS %c", actual);
+
+    return 0;
 }
