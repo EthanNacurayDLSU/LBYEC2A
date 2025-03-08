9d6971c487e152410c6e0d858fd669f1687c4fd1
EthanNacurayDLSU <ethan_nacuray@dlsu.edu.ph>
2025-03-08 18:26:03 +0800

    yes

--- Practical Exam 1-B/inverted/inverted.c
+++ Practical Exam 1-B/inverted/inverted.c
@@ -2,32 +2,39 @@
 
 int main()
 {
-    int h = 0, c, d=0, e=0;//h is height, c is series added to height to determine number of letters in a
+    int h = 0, c, d = 0, e = 0; // h is height, c is series added to height to determine number of letters in a
     char i = 65;
+
     puts("Enter the height of your inverted pyramid");
     scanf("%d", &h);
-    
-    c = h-1;
 
-    while (h>0)
+    // c = h - 1;
+
+    while (h > 0)
     {
-        
+
         puts("");
+
         i = 65;
         e = 0;
-        do //code to add spaced before each new line to center it
+
+        do // code to add spaced before each new line to center it
         {
             printf(" ");
-            e = e+1;
+            e = e + 1;
         } while (e <= d);
-        while ((i-65) < (c+h))
+
+        // 0       9 |   c + h = 4 + 5 | for row 1 height 5
+        while ((i - 65) < (((2 * h) - 1)))
         {
-        printf("%c ", i);
-        i = i+1;
+            printf("%c ", i);
+            i = i + 1;
         }
-        h = h-1;
-        c = c-1;
-        d = d+2;
+
+        h = h - 1;
+        // c = c - 1;
+        d = d + 2;
     }
-return 0;
+
+    return 0;
 }
