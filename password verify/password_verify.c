3b7b6b00666858ad0adc2cffce6a66916bed337b
EthanNacurayDLSU <ethan_nacuray@dlsu.edu.ph>
2025-03-10 11:16:51 +0800

    ??

--- password verify/password_verify.c
+++ password verify/password_verify.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h> //for isdigit
 #include <conio.h>
+//#include "checks.h"
+
+/*Notes : there is no string data type in C, only character arrays*/
 
 int checkupper(char pass[100])
 {
@@ -19,6 +22,58 @@ int checkupper(char pass[100])
     } while (i <= strlen(pass));
     return 1;
 }
+
+int checklower(char pass[100])
+{
+    int i = 0;
+    do
+    {
+        if (islower(pass[i]) == 0)
+        {
+            i++;
+        }
+        else if (islower(pass[i] != 0))
+        {
+            return 0;
+        }
+    } while (i <= strlen(pass));
+    return 1;
+}
+
+int checkdigit(char pass[100])
+{
+    int i = 0;
+    do
+    {
+        if (isdigit(pass[i]) == 0)
+        {
+            i++;
+        }
+        else if (isdigit(pass[i] != 0))
+        {
+            return 0;
+        }
+    } while (i <= strlen(pass));
+    return 1;
+}
+
+int checkspecial(char pass[100])
+{
+    int i = 0;
+    do
+    {
+        if (isalpha(pass[i]) == 0 || isdigit(pass[i]) == 0)
+        {
+            i++;
+        }
+        else
+        {
+            return 0;
+        }
+    } while (i <= strlen(pass));
+    return 1;
+}
+
 
 int main()
 {
@@ -41,19 +96,37 @@ int main()
         strcat(errors, "length (needs at least 10 characters),");
     }
 
-    if (checkupper(password) != 0)
+    if (checkupper(password)!=0)
     {
-        strcat(errors, " uppercase letters,");
+        strcat(errors, " uppercase letter,");
     }
 
-    if (errors == '\0')
+    if(checklower(password)!=0)
+    {
+        strcat(errors, " lowercase letter,");
+    }
+
+   // if(checkdigit(password)!=0)
+   // {
+   //     strcat(errors, " digit,");
+   // }
+    
+    //if(checkspecial(password)!=0)
+   // {
+
+   //     strcat(errors, " special character,");
+   // }
+
+    /*if (errors == '\0')
     {
         printf("No problems");
     }
     else
     {
-        printf("\n%s", errors);
-    }
+        printf("\n Missing : %s", errors);
+    }*/
+
+    printf("\n%s", errors);
 
     return 0;
 }
