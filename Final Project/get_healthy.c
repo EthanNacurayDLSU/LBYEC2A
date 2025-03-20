663e81f668fd6f3a19d113e3d34ccc23fd682d8c
Mosigana <rbmacatangay06@gmail.com>
2025-03-20 10:09:49 +0800

    Integrated BMI into Height and Weight.

--- Final Project/get_healthy.c
+++ Final Project/get_healthy.c
@@ -11,7 +11,7 @@ int main(){
     printf("========================\n"
            "     MENU Options\n"
            "========================\n"
-           "(1) Height and Weight\n"
+           "(1) Height and Weight + BMI\n"
            "(2) BMI value\n"
            "(3) Exercise/Diet Plan\n"
            "(4) Weekly Progress\n");
@@ -21,16 +21,27 @@ int main(){
 
     switch (menu)
     {
-        case 1: //height and weight
-    float BMI, h, w;
+        case 1: //height and weight + BMI
+    float bmi, h, w;
     printf("Please put your height in meters, and weight in kg.\n");
     scanf("%f %f", &h, &w);
-    BMI = w/(h*h);
-    if BMI < 18.5 
-    printf("Your BMI is %.1f", BMI);
+    bmi = w/(h*h);
+    if (bmi < 18.5)
+        printf("Your BMI is %.1f, You are Underweight.", bmi);
+    else if (bmi < 24.9)
+        printf("Your BMI is %.1f, You are Healthy!", bmi);
+    else if (bmi < 29.9)
+        printf("Your BMI is %.1f, You are Overweight.", bmi);
+    else if (bmi < 34.9)
+        printf("Your BMI is %.1f, You are Obese.", bmi);
+    else if (bmi < 39.9)
+        printf("Your BMI is %.1f, You are Severely Obese.", bmi);
+    else
+        printf("Your BMI is %.1f, You are Morbiusly a Beast.", bmi);
+
         break;
         case 2: //bmi value
-
+    
         break;
         case 3: //exercise/diet plan
 
